be2ba1e6efb95fb7ad195426ec3aa68a0d0d00d8
Raphael Leal <raphaelleal990@gmail.com>
2021-09-16 17:30:45 +0000

    Construção

--- Principal.java
+++ Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
-    public static void main(String args[]){
 
+    public static void main(String args[]){
+        
+        Conta minhaConta=new Conta();
+        String nome
         
     }
+
+}
