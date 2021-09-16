aee6668d241fca9f4a14b766d410db281823f916
Raphael Leal <raphaelleal990@gmail.com>
2021-09-16 17:12:49 +0000

    Construção

---
+++ Conta.java
@@ -0,0 +1,14 @@
+public class Conta{
+
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero=numero;
+    }
+}
